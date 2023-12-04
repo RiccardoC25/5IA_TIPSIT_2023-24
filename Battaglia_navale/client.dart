63c268baa9ebab19ab657812088e50de7c8557e4
RiccardoC25 <riccardo.costantini@itiszuccante.edu.it>
2023-12-04 08:54:09 +0100

    Consegna iniziale Battaglia_navale

---
+++ Battaglia_navale/client.dart
@@ -0,0 +1,70 @@
+import 'dart:convert';
+import 'dart:io';
+
+void main(List<String> arguments) async {
+  print('TCP Client');
+  var socket;
+  bool inGame = true;
+  String resServ = "";
+  while(resServ != "Finito"){
+    socket = await Socket.connect('127.0.0.1', 9001);
+    socket.listen((eventBytes) {
+      String result = utf8.decode(eventBytes);
+      resServ = result;
+      result = result.replaceAll("[", "");
+      result = result.substring(0, result.length - 2);
+      List<String> list = result.split("], ");
+
+      if(resServ != "Finito") {
+        print("   A  B  C  D  E  F  G  H  I  J");
+        for (int i = 0; i < list.length; i++) {
+          print(i.toString() + "  " + list[i]);
+        }
+      }
+    });
+
+    print("\n\n\nInserisci la cella dove inserire la nave (Esempio 'A 0') oppure inserisci 'exit' per uscire: ");
+    String? msg = stdin.readLineSync();
+    if(msg == "exit"){
+      break;
+    }
+    socket.add(utf8.encode("P"+msg!));
+    await Future.delayed(Duration(seconds: 2));
+    await socket.flush();
+    socket.destroy();
+  }
+  while(inGame) {
+    socket = await Socket.connect('127.0.0.1', 9001);
+    socket.listen((eventBytes) {
+      String result = utf8.decode(eventBytes);
+      if(result == "You Loose"){
+        print("\n\n\nYou Loose\n\n");
+        inGame = false;
+        return;
+      }else if(result == "You Win"){
+        print("\n\n\nYou Win\n\n");
+        inGame = false;
+        return;
+      }
+      result = result.replaceAll("[", "");
+      result = result.substring(0, result.length - 2);
+      List<String> list = result.split("], ");
+
+      print("   A  B  C  D  E  F  G  H  I  J");
+      for (int i = 0; i < list.length; i++) {
+        print(i.toString() + "  " + list[i]);
+      }
+    });
+
+    print("\n\n\nInserisci la cella (Esempio 'A 0') oppure inserisci 'exit' per uscire: ");
+    String? msg = stdin.readLineSync();
+    if(msg == "exit"){
+      break;
+    }
+    socket.add(utf8.encode("P"+msg!));
+    await Future.delayed(Duration(seconds: 2));
+    await socket.flush();
+    socket.destroy();
+  }
+  socket.destroy();
+}
---
+++ Battaglia_navale/server.dart
@@ -0,0 +1,125 @@
+import 'dart:convert';
+import 'dart:io';
+
+void main(List<String> arguments) async {
+  print('TCP Server');
+  final server = await ServerSocket.bind('0.0.0.0', 9001);
+  var twoDListP1 = List<List>.generate(10, (i) => List<dynamic>.generate(10, (index) => 0, growable: false), growable: false);
+  var twoDListP2 = List<List>.generate(10, (i) => List<dynamic>.generate(10, (index) => 0, growable: false), growable: false);
+
+  var twoDListP1Pos = List<List>.generate(10, (i) => List<dynamic>.generate(10, (index) => 0, growable: false), growable: false);
+  var twoDListP2Pos = List<List>.generate(10, (i) => List<dynamic>.generate(10, (index) => 0, growable: false), growable: false);
+
+  int isP1Pos = 0;
+  int isP2Pos = 0;
+
+  int sinkP1 = 0;
+  int sinkP2 = 0;
+
+  bool isP1 = true;
+
+  server.listen((socket) {
+    socket.listen((eventBytes) {
+      String result = utf8.decode(eventBytes);
+      String player = result.substring(0, 1);
+      result = result.substring(1, result.length);
+      List<String> res = result.split(" ");
+      int row, col = 0;
+      switch(res[0].toUpperCase()){
+        case "A":
+          col = 0;
+          break;
+        case "B":
+          col = 1;
+          break;
+        case "C":
+          col = 2;
+          break;
+        case "D":
+          col = 3;
+          break;
+        case "E":
+          col = 4;
+          break;
+        case "F":
+          col = 5;
+          break;
+        case "G":
+          col = 6;
+          break;
+        case "H":
+          col = 7;
+          break;
+        case "I":
+          col = 8;
+          break;
+        case "J":
+          col = 9;
+          break;
+      }
+      row = int.parse(res[1]);
+
+      if(player.toUpperCase() == "P" && isP1){
+        if(isP1Pos < 5){
+          twoDListP1Pos[row][col] = 2;
+          socket.add(utf8.encode('$twoDListP1Pos'));
+          isP1Pos += 1;
+          print('Nave posizionata = $result');
+          if(isP1Pos-1 != 4) {
+            return;
+          }
+        }
+        isP1 = false;
+        if(isP1Pos-1 == 4) {
+          socket.add(utf8.encode('Finito'));
+          isP1Pos += 1;
+          return;
+        }
+        if(twoDListP2Pos[row][col] == 2){
+          twoDListP1[row][col] = 2;
+          sinkP2++;
+        }else {
+          twoDListP1[row][col] = 1;
+        }
+        if(sinkP1 == 5){
+          socket.add(utf8.encode('You Loose'));
+        }else if(sinkP2 == 5){
+          socket.add(utf8.encode('You Win'));
+        }
+        print('Attacco = $result');
+        socket.add(utf8.encode('$twoDListP1'));
+      }
+
+      if(player.toUpperCase() == "H" && !isP1){
+        if(isP2Pos < 5){
+          twoDListP2Pos[row][col] = 2;
+          socket.add(utf8.encode('$twoDListP2Pos'));
+          isP2Pos += 1;
+          print('Nave posizionata = $result');
+          if(isP2Pos-1 != 4) {
+            return;
+          }
+        }
+        isP1 = true;
+        if(isP2Pos-1 == 4) {
+          socket.add(utf8.encode('Finito'));
+          isP2Pos += 1;
+          return;
+        }
+        if(twoDListP1Pos[row][col] == 2){
+          twoDListP2[row][col] = 2;
+          sinkP1++;
+        }else {
+          twoDListP2[row][col] = 1;
+        }
+        if(sinkP1 == 5){
+          socket.add(utf8.encode('You Win'));
+        }else if(sinkP2 == 5){
+          socket.add(utf8.encode('You Loose'));
+        }
+        print('Attacco = $result');
+        socket.add(utf8.encode('$twoDListP2'));
+      }
+    });
+  });
+}
